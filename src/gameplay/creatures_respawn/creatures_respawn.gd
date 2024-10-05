b4fd530a84268352ff03f5f22dd3582c0374fee4
Moisés J <moisesjbc@gmail.com>
2024-10-05 12:35:00 +0100

    Add creatures respawn

---
+++ src/gameplay/creatures_respawn/creatures_respawn.gd
@@ -0,0 +1,15 @@
+extends Path2D
+
+var creature_scene = preload("res://gameplay/creature/creature.tscn")
+
+
+func _on_respawn_timer_timeout():
+	respawn_creature()
+	
+
+func respawn_creature():
+	randomize()
+	$path_follow.set_unit_offset(randf())
+	var creature = creature_scene.instance()
+	creature.global_position = $path_follow.global_position
+	get_node("/root/main/creatures_respawn").add_child(creature)
