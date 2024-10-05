77226ed311218a2be21d34968aacba3b0f21d89b
Moisés J <moisesjbc@gmail.com>
2024-10-05 15:42:52 +0100

    Fix scanner on from the start

--- src/gameplay/scanner/scanner.gd
+++ src/gameplay/scanner/scanner.gd
@@ -3,18 +3,16 @@ extends Area2D
 
 func _ready():
 	visible = false
-	#set_process(false)
+	toggle_scanner(false)
 
 
 func _process(delta):
 	global_position = get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("scan"):
-		visible = true
-		$collision_shape.disabled = false
+		toggle_scanner(true)
 	elif Input.is_action_just_released("scan"):
-		visible = false
-		$collision_shape.disabled = true
+		toggle_scanner(false)
 
 
 func _on_scanner_body_entered(body):
@@ -25,3 +23,8 @@ func _on_scanner_body_entered(body):
 func _on_scanner_body_exited(body):
 	if body.is_in_group("creatures"):
 		body.hide_feature()
+
+
+func toggle_scanner(scanner_on):
+	visible = scanner_on
+	$collision_shape.disabled = not scanner_on
