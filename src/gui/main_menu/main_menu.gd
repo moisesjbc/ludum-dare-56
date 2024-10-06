d50de0ede1626d3e14136eb33cd5b8fd71e4e261
Moisés J <moisesjbc@gmail.com>
2024-10-06 11:20:47 +0100

    Add exit button to main menu

--- src/gui/main_menu/main_menu.gd
+++ src/gui/main_menu/main_menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
 
+func _ready():
+	$center_container/panel/magin_container/vbox_container/exit_button.visible = (OS.get_name() != "HTML5")
+
+
 func _on_play_button_pressed():
 	get_tree().change_scene("res://gameplay/main/main.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
